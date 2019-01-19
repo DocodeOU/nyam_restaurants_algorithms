55091a6b53446708dc3ddb695bb765e0d41bfe5d
Vlad Mihalachi <themyvlad@gmail.com>
2019-01-19 18:30:56 +0100

    cose

--- models.py
+++ models.py
@@ -28,8 +28,11 @@ class Pizza:
         self.ingredients: List[Ingredient] = [Ingredient(json=x) for x in json['ingredients']]
         self.pizza_options: List[PizzaOption] = [PizzaOption(json=x) for x in json['pizza_options']]
         self.type: int = json['type']
-        self.key_without_pizza_options: str = json['key_without_pizza_options']
         self.quantity: int = json['quantity']
+
+        @property
+        def key_without_pizza_options(self):
+            return None
 
 
 class Drink:
