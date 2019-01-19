2617ad25d944cda2b27667c272edee45e1a75f10
Vlad Mihalachi <themyvlad@gmail.com>
2019-01-19 18:58:24 +0100

    cose

--- pizziamo_net/database.py
+++ pizziamo_net/database.py
@@ -4,16 +4,13 @@ from ..models import Consts, Pizza, Drink, Dessert, Ingredient, PizzaOption
 
 class DatabasePizziamoNet(AbstractDatabase):
     
-    def __init__(self):
-        consts = Consts(json={
-            "ingredient_pomodoro": 48,
-            "ingredient_mozzarella": 49
-        })
-        pizzas = [Pizza(json=x) for x in []]
-        drinks = [Drink(json=x) for x in []]
-        desserts = [Dessert(json=x) for x in []]
-        ingredients = [Ingredient(json=x) for x in []]
-        pizza_options = [PizzaOption(json=x) for x in []]
+    def __init__(self, json):
+        consts = Consts(json=json['consts'])
+        pizzas = [Pizza(json=x) for x in json['pizzas']]
+        drinks = [Drink(json=x) for x in json['drinks']]
+        desserts = [Dessert(json=x) for x in json['desserts']]
+        ingredients = [Ingredient(json=x) for x in json['ingredients']]
+        pizza_options = [PizzaOption(json=x) for x in json['pizza_options']]
         
         super().__init__(consts=consts, pizzas=pizzas, drinks=drinks, desserts=desserts, ingredients=ingredients,
                          pizza_options=pizza_options)
