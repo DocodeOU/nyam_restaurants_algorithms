5256538b64c747e17cb8fc3c96a8d9cb1cbe7a49
Vlad Mihalachi <themyvlad@gmail.com>
2019-01-20 12:37:56 +0100

    cose

--- app/models.py
+++ app/models.py
@@ -3,10 +3,10 @@ from typing import List
 
 class Ingredient:
     def __init__(self, json):
-        self.id: int = int(json['id'])
+        self.id: int = json['id']
         self.name: str = json['name']
-        self.price: float = float(json['price'])
-        self.quantity: int = int(json['quantity'])
+        self.price: float = json['price']
+        self.quantity: int = json['quantity']
         self.cooked_in_oven_by_default: bool = json['cooked_in_oven_by_default']
         self.uscita: bool = json['uscita'] if 'uscita' in json else not self.cooked_in_oven_by_default
         self.just_a_little: bool = json['just_a_little'] if 'just_a_little' in json else False
@@ -14,20 +14,20 @@ class Ingredient:
 
 class PizzaOption:
     def __init__(self, json):
-        self.id: int = int(json['id'])
+        self.id: int = json['id']
         self.name: str = json['name']
-        self.price: float = float(json['price'])
+        self.price: float = json['price']
 
 
 class Pizza:
     def __init__(self, json):
-        self.id: int = int(json['id'])
+        self.id: int = json['id']
         self.name: str = json['name']
-        self.price: float = float(json['price'])
+        self.price: float = json['price']
         self.ingredients: List[Ingredient] = [Ingredient(json=x) for x in json['ingredients']]
         self.pizza_options: List[PizzaOption] = [PizzaOption(json=x) for x in json['pizza_options']]
-        self.type: int = int(json['type'])
-        self.quantity: int = int(json['quantity'])
+        self.type: int = json['type']
+        self.quantity: int = json['quantity']
     
     @property
     def key_without_pizza_options(self):
@@ -50,17 +50,17 @@ class Pizza:
 
 class DeliveryType:
     def __init__(self, json):
-        self.id: int = int(json['id'])
+        self.id: int = json['id']
         self.name: str = json['name']
 
 
 class TypeOfPizza:
     def __init__(self, json):
-        self.id: int = int(json['id'])
+        self.id: int = json['id']
         self.name: str = json['name']
 
 
 class Consts:
     def __init__(self, json):
-        self.ingredient_mozzarella: int = int(json['ingredient_mozzarella'])
-        self.ingredient_pomodoro: int = int(json['ingredient_pomodoro'])
+        self.ingredient_mozzarella: int = json['ingredient_mozzarella']
+        self.ingredient_pomodoro: int = json['ingredient_pomodoro']
