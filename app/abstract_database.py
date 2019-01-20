0f719a1d4facb484432a0f81a178846d0b8995ca
Vlad Mihalachi <themyvlad@gmail.com>
2019-01-20 17:53:31 +0100

    database

--- app/abstract_database.py
+++ app/abstract_database.py
@@ -4,16 +4,16 @@ from .models import Consts, Pizza, Ingredient, PizzaOption
 
 
 class AbstractDatabase:
-    def __init__(self, consts: Consts, pizzas: List[Pizza], ingredients: List[Ingredient],
-                 pizza_options: List[PizzaOption]):
-        self.consts = consts
-        self.pizzas = pizzas
-        self.ingredients = ingredients
-        self.pizza_options = pizza_options
-        
+    def __init__(self, json: dict):
+        self.consts = Consts(json=json['consts'])
+        self.pizzas = [Pizza(json=x) for x in json['pizzas']]
+        self.ingredients = [Ingredient(json=x) for x in json['ingredients']]
+        self.pizza_options = [PizzaOption(json=x) for x in json['pizza_options']]
+
         self.delivery_type_home = 2
         self.type_pizza = 1
         self.type_cecio = 3
+        
         # self.delivery_type_home = next(x for x in delivery_types if x.name == 'Domicilio')
         # self.type_pizza = next(x for x in types_of_pizzas if x.name == 'Pizza')
         # self.type_cecio = next(x for x in types_of_pizzas if x.name == 'Cecio')
--- pizziamo_net/database.py
+++ pizziamo_net/database.py
@@ -1,14 +1,6 @@
 from nyam_restaurants_algorithms.app.abstract_database import AbstractDatabase
-from nyam_restaurants_algorithms.app.models import Consts, Pizza, Ingredient, PizzaOption
-
 
 class DatabasePizziamoNet(AbstractDatabase):
     
     def __init__(self, json):
-        consts = Consts(json=json['consts'])
-        pizzas = [Pizza(json=x) for x in json['pizzas']]
-        ingredients = [Ingredient(json=x) for x in json['ingredients']]
-        pizza_options = [PizzaOption(json=x) for x in json['pizza_options']]
-        
-        super().__init__(consts=consts, pizzas=pizzas, ingredients=ingredients,
-                         pizza_options=pizza_options)
+        super().__init__(json=json)
