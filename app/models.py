62bbcec487861a80d2a9d0354cdf196216a43619
Vlad Mihalachi <themyvlad@gmail.com>
2019-01-20 18:05:21 +0100

    database

--- app/models.py
+++ app/models.py
@@ -6,7 +6,7 @@ class Ingredient:
         self.id: int = json['id']
         self.name: str = json['name']
         self.price: float = json['price']
-        self.quantity: int = json['quantity']
+        self.quantity: int = json['quantity'] if 'quantity' in json else 1
         self.cooked_in_oven_by_default: bool = json['cooked_in_oven_by_default']
         self.uscita: bool = json['uscita'] if 'uscita' in json else not self.cooked_in_oven_by_default
         self.just_a_little: bool = json['just_a_little'] if 'just_a_little' in json else False
@@ -27,7 +27,7 @@ class Pizza:
         self.ingredients: List[Ingredient] = [Ingredient(json=x) for x in json['ingredients']]
         self.pizza_options: List[PizzaOption] = [PizzaOption(json=x) for x in json['pizza_options']]
         self.type: int = json['type']
-        self.quantity: int = json['quantity']
+        self.quantity: int = json['quantity'] if 'quantity' in json else 1
     
     @property
     def key_without_pizza_options(self):
