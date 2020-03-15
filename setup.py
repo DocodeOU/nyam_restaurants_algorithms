89836dbe279c87e6f03fbef95e92765984da4dce
vmihalachi <vmihalachi@outlook.com>
2020-03-15 16:09:47 +0200

    packages

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 NAME = 'nyam_restaurants_algorithms'
 DESCRIPTION = 'Alghorithms for names and prices of restaurants'
-VERSION = '1.0.0'
+VERSION = '1.0.1'
 
 setup(
     description=DESCRIPTION,
